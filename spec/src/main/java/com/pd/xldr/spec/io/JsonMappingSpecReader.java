1962def923228ab5970e03d87a0fbae84502f52d
ralfspoeth <ralf.spoeth@gmail.com>
2024-07-30 12:19:14 +0200

    wip

--- spec/src/main/java/com/pd/xldr/spec/io/JsonMappingSpecReader.java
+++ spec/src/main/java/com/pd/xldr/spec/io/JsonMappingSpecReader.java
@@ -2,11 +2,13 @@ package com.pd.xldr.spec.io;
 
 import com.pd.xldr.spec.*;
 import io.github.ralfspoeth.json.JsonObject;
-import io.github.ralfspoeth.json.conv.StandardConversions;
 import io.github.ralfspoeth.json.io.JsonReader;
 
 import java.io.IOException;
 import java.io.Reader;
+
+import static io.github.ralfspoeth.json.query.Queries.members;
+import static io.github.ralfspoeth.json.query.Queries.stringValue;
 
 public class JsonMappingSpecReader implements MappingSpecReader {
 
@@ -14,7 +16,9 @@ public class JsonMappingSpecReader implements MappingSpecReader {
     public MappingSpec readFrom(Reader src) throws IOException {
         try(var jsonRdr = new JsonReader(src)) {
             return switch(jsonRdr.readElement()) {
-                case JsonObject jo -> StandardConversions.as(MappingSpec.class, jo);
+                case JsonObject jo -> new MappingSpec(
+                        new InputSpec(stringValue(members(jo).get("input")))
+                );
                 case null, default -> throw new IllegalArgumentException("source not a JSON object");
             };
         }
