0c869f633689d747c83545f5c758b65787e959d1
Ralf Spöth <ralf.spoeth@profidata.com>
2024-11-20 01:44:49 +0100

    wip, works again

--- spec/src/main/java/module-info.java
+++ spec/src/main/java/module-info.java
@@ -1,8 +1,11 @@
+import com.pd.xldr.spec.io.*;
+
 module com.pd.xldr.spec {
     exports com.pd.xldr.spec;
     exports com.pd.xldr.spec.io;
     requires io.github.ralfspoeth.json;
     requires io.github.ralfspoeth.dirs;
+    requires jdk.jshell;
 
     uses MappingSpecReader;
     provides MappingSpecReader with JsonMappingSpecReader, PropertiesMappingSpecReader;
