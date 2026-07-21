5d983eae2da01164cd23cb46596844ab7faa5b4c
ralfspoeth <ralf.spoeth@gmail.com>
2026-07-21 22:54:22 +0200

    spec now includes an xml spec reader

---
+++ spec/src/main/java/com/pd/xldr/spec/io/XmlMappingSpecReader.java
@@ -0,0 +1,57 @@
+package com.pd.xldr.spec.io;
+
+import com.pd.xldr.spec.*;
+import io.github.ralfspoeth.xmls.Xml;
+import io.github.ralfspoeth.xmls.XmlFunctions;
+import org.w3c.dom.Element;
+
+import java.io.Reader;
+
+
+public class XmlMappingSpecReader implements MappingSpecReader {
+
+    @Override
+    public MappingSpec readFrom(Reader source) {
+        try {
+            var doc = Xml.parse(source);
+            var root = doc.getDocumentElement();
+            return new MappingSpec(
+                    inputSpec(XmlFunctions.elements("input").apply(root).findFirst().orElseThrow()),
+                    XmlFunctions.elements("mapping")
+                            .apply(root)
+                            .map(XmlMappingSpecReader::recordMappingSpec)
+                            .toList(),
+                    outputSpec(XmlFunctions.elements("output").apply(root).findFirst().orElseThrow())
+            );
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static InputSpec inputSpec(Element input) {
+        return new InputSpec(
+                input.getAttribute("mimeType"),
+                null// recordSelectors
+        );
+    }
+
+    private static OutputSpec outputSpec(Element output) {
+        return new OutputSpec(
+                output.getAttribute("url"), null
+        );
+    }
+
+    private static RecordMappingSpec recordMappingSpec(Element mapping) {
+        return new RecordMappingSpec(
+                mapping.getAttribute("recordSelector"),
+                mapping.getAttribute("databaseTable"),
+                XmlFunctions.elements("fieldMapping")
+                        .apply(mapping)
+                        .map(fm -> new FieldMappingSpec(
+                                fm.getAttribute("fieldName"),
+                                fm.getAttribute("databaseColumnName")
+                        ))
+                        .toList()
+        );
+    }
+}
--- spec/src/main/java/module-info.java
+++ spec/src/main/java/module-info.java
@@ -1,12 +1,15 @@
 import com.pd.xldr.spec.io.JsonMappingSpecReader;
 import com.pd.xldr.spec.io.MappingSpecReader;
 import com.pd.xldr.spec.io.PropertiesMappingSpecReader;
+import com.pd.xldr.spec.io.XmlMappingSpecReader;
 
 module com.pd.xldr.spec {
     exports com.pd.xldr.spec;
     exports com.pd.xldr.spec.io;
     requires io.github.ralfspoeth.greyson;
     requires io.github.ralfspoeth.dirs;
+    requires io.github.ralfspoeth.xmls;
+    requires java.xml;
 
-    provides MappingSpecReader with JsonMappingSpecReader, PropertiesMappingSpecReader;
+    provides MappingSpecReader with JsonMappingSpecReader, PropertiesMappingSpecReader, XmlMappingSpecReader;
 }
